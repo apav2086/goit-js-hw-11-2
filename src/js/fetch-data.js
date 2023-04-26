a4163a750cce1266bec632834ec054a767836abf
apav2086 <apav2086@gmail.com>
2023-04-26 10:21:59 -0400

    adding axios

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -1,4 +1,5 @@
-export default function fetchImages(page) {
+import axios, { Axios } from 'axios';
+export default async function fetchImages(page) {
     const options = {
         key: '35585241-0d017fc6894dff5aad1093c8d',
         enteredValue: document.querySelector('input').value,
@@ -8,7 +9,6 @@ export default function fetchImages(page) {
         perPage: 40,
     };
     const url = `https://pixabay.com/api?key=${options.key}&q=${options.enteredValue}&image_type=${options.imageType}&orientation=${options.orientation}&safesearch=${options.safeSearch}&per_page=${options.perPage}&page${page}`;
-    return fetch(url).then(response => {
-        return response.json();
-    });
+    const response = await axios.get(url);
+    return response.data;
 }
